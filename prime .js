c51f811cb137ed196124ca0c1a9e66516540c499
chandrakala <chandhu6302886523@gmail.com>
2023-01-15 16:53:02 +0530

    error raiesd

---
+++ prime .js
@@ -0,0 +1,17 @@
+//check the number is prime or not
+
+function primenum(){
+let factor=0;
+let num=13;
+for(let i=1;i<=num;i++){
+  if(num%i===0){
+    factor++;
+  }
+}
+if(factor==2){
+  console.log("Yes")
+} 
+else{
+  console.log("no")
+}
+}
